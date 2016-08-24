f6fade1070aa6939a2e9b65e4732d6f855694af7
Mihály Hunyady <megamegax@gmail.com>
2016-08-24 09:06:33 +0200

    fixing hibernate issues

--- src/main/kotlin/com/epam/agentx/room/model/Room.kt
+++ src/main/kotlin/com/epam/agentx/room/model/Room.kt
@@ -4,6 +4,7 @@ import com.epam.agentx.user.model.User
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToMany
 
 @Entity
-data class Room(@Id @GeneratedValue val id: Long, val name: String, val users: List<User>)
+data class Room(@Id @GeneratedValue val id: Long, val name: String, @OneToMany val users: List<User>)
--- src/main/kotlin/com/epam/agentx/user/model/User.kt
+++ src/main/kotlin/com/epam/agentx/user/model/User.kt
@@ -4,6 +4,7 @@ import com.epam.agentx.statistic.model.Statistic
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToOne
 
 @Entity
-data class User(@Id @GeneratedValue val id: Long, val displayName: String, val username: String, val password: String, val statistics: Statistic)
+data class User(@Id @GeneratedValue val id: Long, val displayName: String, val username: String, val password: String, @OneToOne val statistics: Statistic)
