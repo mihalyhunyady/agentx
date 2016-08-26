a995c2f4962156e6410309f013c64cf40d92c282
Mihály Hunyady <megamegax@gmail.com>
2016-08-26 15:57:34 +0200

    bugfix

--- src/main/kotlin/com/epam/agentx/room/model/Room.kt
+++ src/main/kotlin/com/epam/agentx/room/model/Room.kt
@@ -1,12 +1,10 @@
 package com.epam.agentx.room.model
 
 import com.epam.agentx.user.model.User
-import org.hibernate.annotations.Cascade
 import java.util.*
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
-import javax.persistence.OneToMany
 
 @Entity
-data class Room(@Id @GeneratedValue val id: Long = 0, val name: String = "", @Cascade(value = CascadeType.SAVE_UPDATE) @OneToMany var users: ArrayList<User> = ArrayList())
+data class Room(@Id @GeneratedValue val id: Long = 0, val name: String = "", var users: ArrayList<User> = ArrayList())
