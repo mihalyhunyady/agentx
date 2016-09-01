10f91395f22992df2145b26112cc675f82990841
Mihály Hunyady <megamegax@gmail.com>
2016-09-01 10:48:58 +0200

    refactored

--- src/main/kotlin/com/epam/agentx/game/service/GameService.kt
+++ src/main/kotlin/com/epam/agentx/game/service/GameService.kt
@@ -4,11 +4,11 @@ import com.epam.agentx.map.model.MapCell
 import com.epam.agentx.user.model.User
 
 interface GameService {
-	
-    fun startGame(, lambda:()->Unit)
-	
-	fun selectCell(user : User, mapRow : MapCell, lambda:(Any)->Unit)
-	
-	fun sendAssociation(user : User, word : String, lambda:(Any)->Unit)
-	
+
+    fun startGame(lambda: () -> Unit)
+
+    fun selectCell(user: User, mapRow: MapCell, lambda: (Any) -> Unit)
+
+    fun sendAssociation(user: User, word: String, number: Int, lambda: (Any) -> Unit)
+
 }
